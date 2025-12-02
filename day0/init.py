98e022a898522ae4094e05ef9afb6757ed538f9c
picheryc <16350216+karu9@users.noreply.github.com>
2025-12-02 09:19:18 +0100

    revamp boilerplate

--- day0/init.py
+++ day0/init.py
@@ -1,3 +1,4 @@
+import os
 from common.file_utils import example_as_lines, input_as_lines
 
 
@@ -14,7 +15,16 @@ def process_input(input):
 
 
 if __name__ == '__main__':
-    example = example_as_lines("day0")
-    input = input_as_lines("day0")
+    parent_folder = os.path.basename(os.path.dirname(__file__))
+    example = example_as_lines(parent_folder)
+    input = input_as_lines(parent_folder)
 
+    print("example part_1")
     print(part_1(process_input(example)))
+    print("input part_1")
+    print(part_1(process_input(input)))
+
+    print("example part_2")
+    print(part_2(process_input(example)))
+    print("input part_2")
+    print(part_2(process_input(input)))
